d16b8d6984727fbf05a6fea0d0d757f4fa4060c7
Crowdbotics <team@crowdbotics.com>
2021-08-26 10:49:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rnwebviewapp_30121Identifier",
+  url: "https://rnwebviewapp_30121Identifier.botics.co",
+  api: "https://rnwebviewapp_30121Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
